1aefcaefe4bfdaa6137d1f4f8e92dc65eb9760a7
Hamdi Damar <hmdmr1@hotmail.com>
2021-09-07 13:54:10 +0300

    fisrt commit
    temel katmanlar ve varlık sınıfları oluşturuldu

---
+++ CoreCamp/Entities/Concrete/Comment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Comment
+    {
+        public int CommentId { get; set; }
+        public string CommentUsername { get; set; }
+        public string CommentTitle { get; set; }
+        public string CommentContent { get; set; }
+        public DateTime CommentDate { get; set; }
+        public bool CommentStatus { get; set; }
+
+    }
+}
